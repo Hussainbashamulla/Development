06210665df85ff351122317ce7728091cc37a63b
Hussainbashamulla <hussainrevanur@gmail.com>
2017-03-25 21:43:44 +0530

    Update Binary.java

--- basics/src/basics/Binary.java
+++ basics/src/basics/Binary.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Binary {
 	public static void main(String[] args) {
 int a=0b1000;
-System.out.println(a);		
+System.out.println(a);	
+		System.out.println(a);	
 	}
 
 }
